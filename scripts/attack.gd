a48e9525d31c4b18a1bb5667d92fe443386246c9
mustardmouth <85802492+mustardmouth@users.noreply.github.com>
2024-09-18 20:01:36 -0600

    changes to tower and addition of wizard

--- scripts/attack.gd
+++ scripts/attack.gd
@@ -9,3 +9,7 @@ func _physics_process(delta: float) -> void:
 func _on_tower_attack_hurtbox_area_entered(area: Area2D) -> void:
 	if area is HitBox:
 		queue_free()
+@onready var sprite_2d: Sprite2D = %Sprite2D
+
+func _ready() -> void:
+	sprite_2d.look_at(velocity+global_position)
