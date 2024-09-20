5d9631a99d18a1c7d53f40c8116a7cfd14ceba83
digiur <digiur@digiur.dev>
2024-09-20 12:04:08 -0600

    Cleanup some stuff and add camera to player

--- scripts/hitbox.gd
+++
@@ -1,8 +0,0 @@
-class_name HitBox extends Area2D
-
-@onready var health_points: HealthPoints = %HealthPoints
-
-signal hit
-
-func _on_area_entered(area: Area2D) -> void:
-	print("Hitbox collided with Hurtbox")
