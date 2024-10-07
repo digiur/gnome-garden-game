b53e408379f92f849a2ac362ee63a4494d422e46
digiur <digiur@digiur.dev>
2024-10-07 16:43:49 -0600

    janky lights

--- scripts/debug_camera.gd
+++ scripts/debug_camera.gd
@@ -2,6 +2,7 @@ extends Camera2D
 
 @export var last_pos: Vector2 = Vector2.ZERO
 @export var speed: float = 2500.0
+@export var zoom_speed: float = 1.1
 
 func _ready() -> void:
 	last_pos = position
@@ -10,8 +11,21 @@ func _process(delta: float) -> void:
 	GGG.player_move.emit(global_position - last_pos)
 
 	last_pos = global_position
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("camera_reset"):
+		position = Vector2.ZERO
+		zoom = Vector2.ONE
 
-func _physics_process(delta):
+	if event.is_action_pressed("camera_zoom_in"):
+		zoom *= zoom_speed
+
+	if event.is_action_pressed("camera_zoom_out"):
+		zoom *= 1.0/zoom_speed
+		
+	zoom = clamp(zoom, Vector2(0.25, 0.25), Vector2(3.0, 3.0))
+
+func _physics_process(delta: float):
 	if Input.is_action_pressed("camera_down"):
 		position += Vector2.DOWN * speed * delta
 
@@ -23,6 +37,3 @@ func _physics_process(delta):
 
 	if Input.is_action_pressed("camera_left"):
 		position += Vector2.LEFT * speed * delta
-
-	if Input.is_action_pressed("camera_reset"):
-		position = Vector2.ZERO
--- scripts/particles_tile.gd
+++ scripts/particles_tile.gd
@@ -1,5 +1,6 @@
 class_name ParticlesTile extends Node2D
 
+@export var explode: = true
 @export var one_shot: = true:
 	set(v):
 		one_shot = v
@@ -14,32 +15,42 @@ class_name ParticlesTile extends Node2D
 @onready var motes_b: GPUParticles2D = %MotesB
 @onready var motes_c: GPUParticles2D = %MotesC
 @onready var gpu_particles_2d: GPUParticles2D = $GPUParticles2D
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = %AudioStreamPlayer2D
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
 
 func _ready() -> void:
 	restart()
 
 func restart() -> void:
 	core.restart()
-	core.one_shot = one_shot
+	core.one_shot = false
 	base_a.restart()
 	base_a.one_shot = false
 	base_b.restart()
 	base_b.one_shot = false
 	base_c.restart()
-	base_c.one_shot = one_shot
+	base_c.one_shot = false
 	motes_a.restart()
 	motes_a.one_shot = false
 	motes_b.restart()
 	motes_b.one_shot = false
 	motes_c.restart()
-	motes_c.one_shot = one_shot
+	motes_c.one_shot = false
 	gpu_particles_2d.restart()
 	gpu_particles_2d.one_shot = true
+	
+	if one_shot:
+		stop()
+
+	if !explode:
+		audio_stream_player_2d.stop()
+		gpu_particles_2d.visible = false
 
 func stop() -> void:
 	core.one_shot = true
 	motes_c.one_shot = true
 	base_c.one_shot = true
+	animation_player.active = true
 
 func _on_core_finished() -> void:
 	base_a.one_shot = true
